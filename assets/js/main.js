883bc68231c5b71a22ffd5a161ba06d7b638452b
Seyed Mahdi Hosseini <smhosseini96@outlook.com>
2022-10-30 13:37:28 +0330

    built home and added slider. add scroll-header function.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,55 @@
 /*=============== SHOW MENU ===============*/
 
-
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
-
 /*=============== SHOW CART ===============*/
-
 
 /*===== CART SHOW =====*/
 /* Validate if constant exists */
 
-
 /*===== CART HIDDEN =====*/
 /* Validate if constant exists */
 
-
 /*=============== SHOW LOGIN ===============*/
-
 
 /*===== LOGIN SHOW =====*/
 /* Validate if constant exists */
 
-
 /*===== LOGIN HIDDEN =====*/
 /* Validate if constant exists */
 
-
 /*=============== HOME SWIPER ===============*/
 
+var homeSwiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  loop: "true",
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
+function scrollHeader() {
+  const header = document.getElementById('header')
+  // when the scroll is greater than 50 viewport height, add scroll-header to the header tag
+  if(this.scrollY >= 50) header.classList.add('scroll-header');else header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== NEW SWIPER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== LIGHT BOX ===============*/
 
-
 /*=============== QUESTIONS ACCORDION ===============*/
-
 
 /*=============== STYLE SWITCHER ===============*/
