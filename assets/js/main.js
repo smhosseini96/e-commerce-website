c8ea3217e54314dec3ea11b9f999911e485e7463
Seyed Mahdi Hosseini <smhosseini96@outlook.com>
2022-11-01 13:24:07 +0330

    scroll-top button. login button, cart button, scroll-up button function.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,19 +8,47 @@
 
 /*=============== SHOW CART ===============*/
 
+const cart = document.getElementById("cart"),
+  cartShop = document.getElementById("cart-shop"),
+  cartClose = document.getElementById("cart-close");
+
 /*===== CART SHOW =====*/
 /* Validate if constant exists */
+if (cartShop) {
+  cartShop.addEventListener("click", () => {
+    cart.classList.add("show-cart");
+  });
+}
 
 /*===== CART HIDDEN =====*/
 /* Validate if constant exists */
+if (cartClose) {
+  cartClose.addEventListener("click", () => {
+    cart.classList.remove("show-cart");
+  });
+}
 
 /*=============== SHOW LOGIN ===============*/
 
+const login = document.getElementById("login"),
+  loginButton = document.getElementById("login-button"),
+  loginClose = document.getElementById("login-close");
+
 /*===== LOGIN SHOW =====*/
 /* Validate if constant exists */
+if (loginButton) {
+  loginButton.addEventListener("click", () => {
+    login.classList.add("show-login");
+  });
+}
 
 /*===== LOGIN HIDDEN =====*/
 /* Validate if constant exists */
+if (loginClose) {
+  loginClose.addEventListener("click", () => {
+    login.classList.remove("show-login");
+  });
+}
 
 /*=============== HOME SWIPER ===============*/
 
@@ -55,6 +83,14 @@ var newSwiper = new Swiper(".new-swiper", {
 });
 
 /*=============== SHOW SCROLL UP ===============*/
+
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // when the scroll is higher than 350 viewport height, add the show-scroll class to a tag with the scroll-top class
+  if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
 
 /*=============== LIGHT BOX ===============*/
 
