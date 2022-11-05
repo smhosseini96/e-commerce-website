102844316a4fe2349574bf0aeb886505ae31ba94
Seyed Mahdi Hosseini <smhosseini96@outlook.com>
2022-11-06 01:25:42 +0330

    built style switcher.

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,3 +139,52 @@ const toggleItem = (item) => {
 };
 
 /*=============== STYLE SWITCHER ===============*/
+
+const styleSwitcherToggle = document.querySelector(".style__switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+  document.querySelector(".style__switcher").classList.toggle("open");
+});
+
+// Hide switcher on scroll
+window.addEventListener("scroll", () => {
+  if (document.querySelector(".style__switcher").classList.contains("open")) {
+    document.querySelector(".style__switcher").classList.remove("open");
+  }
+});
+
+function themeColors() {
+  const colorStyle = document.querySelector(".js-color-style"),
+    themeColorsContainer = document.querySelector(".js-theme-colors");
+  themeColorsContainer.addEventListener("click", ({ target }) => {
+    if (target.classList.contains("js-theme-color-item")) {
+      localStorage.setItem("color", target.getAttribute("data-js-theme-color"));
+      setColors();
+    }
+  });
+  function setColors() {
+    let path = colorStyle.getAttribute("href").split("/");
+    path = path.slice(0, path.length - 1);
+    colorStyle.setAttribute(
+      "href",
+      path.join("/") + "/" + localStorage.getItem("color") + ".css"
+    );
+    if (document.querySelector(".js-theme-color-item.active")) {
+      document
+        .querySelector(".js-theme-color-item.active")
+        .classList.remove("active");
+    }
+    document
+      .querySelector(
+        "[data-js-theme-color=" + localStorage.getItem("color") + "]"
+      )
+      .classList.add("active");
+  }
+  if(localStorage.getItem("color") !== null){
+    setColors();
+  } else {
+    const defaultColor = colorStyle.getAttribute("href").split("/").pop().split(".").shift()
+    document.querySelector("[data-js-theme-color" + defaultColor + "]").classList.add("active")
+  }
+}
+
+themeColors();
